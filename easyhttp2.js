32b6e2eda291bcef2f86ac0e51c82ca759ad202d
Deepak Guptha <vzan2012@gmail.com>
2019-06-02 16:53:18 +0530

    feat(users): CRUD operation of users has been updated

---
+++ easyhttp2.js
@@ -0,0 +1,65 @@
+/**
+ * EasyHTTP2 Library
+ *
+ * @ Author: Sitharaman Deepak Guptha
+ * @ Description: Library for making the HTTP Requests
+ */
+
+class EasyHTTP {
+  // Make a HTTP Get Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(resp => resp.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make a HTTP Post Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(resp => resp.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make a HTTP PUT Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(resp => resp.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make a HTTP Delete Request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(() => resolve("Resource Deleted..."))
+        .catch(err => reject(err));
+    });
+  }
+}
